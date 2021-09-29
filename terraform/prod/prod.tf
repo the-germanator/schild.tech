0e9c992960448348b99e2c4bbdcbae2264830290
Christoph Schild <cvs46@nau.edu>
2021-09-28 17:21:11 -0700

    New flowgit add .

---
+++ terraform/prod/prod.tf
@@ -0,0 +1,36 @@
+terraform {
+   backend "s3" {
+    bucket = "schild-tech-tf"
+    key = "tf-test"
+    region = "us-east-1"
+    endpoint = "nyc3.digitaloceanspaces.com"
+    skip_requesting_account_id = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms = true
+    skip_metadata_api_check = true
+  }
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "prod" {
+  image  = "ubuntu-18-04-x64"
+  name   = "prod"
+  region = "nyc3"
+  size   = "s-2vcpu-2gb"
+  ssh_keys = [ "31420189" ]
+}
+
+resource "digitalocean_floating_ip_assignment" "prod_floating_ips" {
+  ip_address = "138.197.55.27"
+  droplet_id = digitalocean_droplet.prod.id
+}

5e19436bedd4e7ee736b758a459494d66d1a63dd
Christoph Schild <cvs46@nau.edu>
2021-09-28 17:22:11 -0700

    fix bad ignore flags for s3

--- terraform/prod/prod.tf
+++ terraform/prod/prod.tf
@@ -4,9 +4,7 @@ terraform {
     key = "tf-test"
     region = "us-east-1"
     endpoint = "nyc3.digitaloceanspaces.com"
-    skip_requesting_account_id = true
     skip_credentials_validation = true
-    skip_get_ec2_platforms = true
     skip_metadata_api_check = true
   }
   required_providers {

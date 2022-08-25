dd8642cc22956ac011a21bfaf66ffe1fde1646d9
Christoph Schild <cvs46@nau.edu>
2022-08-25 14:23:01 -0400

    Updated Physical Location

--- html/cmds.js
+++ html/cmds.js
@@ -1,7 +1,7 @@
 const commands = [
     {in: 'ls', out: `<br />total 1<br />-rw-r--r-- 1 christoph christoph 171 ${datestring} hello_world<br /><br />`},
     {in: 'chmod +x hello_world', out: '<br />'},
-    {in: './hello_world', out: '<br />Hello, World. My name is Christoph Schild and I\'m a Software Developer from Tempe, AZ.<br /><br /> \
+    {in: './hello_world', out: '<br />Hello, World. My name is Christoph Schild and I\'m a Software Engineer living in Atlanta, GA.<br /><br /> \
         Please select one of the following to continue:<br /> \
         <a target="_BLANK" href=\'christoph_schild_resume_2021.pdf\'>Resume</a>&nbsp; \
         <a target="_BLANK" href=\'https://linkedin.com/in/germanator\'>Linkedin</a>&nbsp; \
